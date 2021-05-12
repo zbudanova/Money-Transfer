a717c8d3ef0add14081c8d436c198686fd537e2e
Zarina Budanova <zarina.nurbaeva@gmail.com>
2021-05-12 22:56:21 +0500

    Pay to balance

--- scr/Main.java
+++ scr/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int pay = 2_000_000_000;
+        int balance = 2_000_000_000;
         int transfer = 500_000_000;
-        int total = pay + transfer;
+        int total = balance + transfer;
         System.out.println(total);
     }
 }
