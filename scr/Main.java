f75e722d413364626eaab48d2e923f34e18ab699
Zarina Budanova <zarina.nurbaeva@gmail.com>
2021-05-12 22:51:36 +0500

    App money Transfer

---
+++ scr/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int pay = 2_000_000_000;
+        int transfer = 500_000_000;
+        int total = pay + transfer;
+        System.out.println(total);
+    }
+}
